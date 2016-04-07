8e129d4d7bb3cd0b23d1a05c95871f40fcf7cfae
Bill Jameson <williamjameson@alum.rpi.edu>
2016-04-07 00:42:29 -0400

    Generate simple report showing only relevant changes
    TODO: line numbers/context in source files

--- edit_police.py
+++ edit_police.py
@@ -4,6 +4,21 @@ diff backups of cnc programs to sniff out manual editing
 v0.2: prototype
 '''
 import difflib
+import re
+
+def line_check(line):
+    '''Return True if the line is not common to both inputs, False otherwise
+    (or if the line should be ignored regardless)'''
+    # ignore commands beginning with E variables
+    match_pattern = '^[+-?].*'
+    ignore_pattern = '^..E.+'
+    match = re.match(match_pattern, line)
+    ignore = re.match(ignore_pattern, line)
+
+    if match and not ignore:
+        return True
+    else:
+        return False
 
 def diff_files(prev_filename, new_filename):
     prev_backup = open(prev_filename)
@@ -12,10 +27,15 @@ def diff_files(prev_filename, new_filename):
     new_backup = open(new_filename)
     new_backup_contents = new_backup.read().splitlines()
     new_backup.close()
-    diff = difflib.unified_diff(prev_backup_contents, new_backup_contents,
-                                fromfile=prev_filename, tofile=new_filename)
 
-    return [d for d in diff]
+    d = difflib.Differ()
+    return list(d.compare(prev_backup_contents, new_backup_contents))
 
 testdata = ('108_2mockup.NC', '108_3modified.NC')
-deltas = diff_files(testdata[0], testdata[1])
+diff = diff_files(testdata[0], testdata[1])
+
+report = [line for line in diff if line_check(line)]
+
+for line in report:
+    print(line)
+
